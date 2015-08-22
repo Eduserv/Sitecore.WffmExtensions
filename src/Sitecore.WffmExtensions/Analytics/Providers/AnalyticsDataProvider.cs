00647dac10864d79f55bedac72d300eb5da34f2b
ajs <adam.simmonds@eduserv.org.uk>
2015-08-22 16:10:52 +0100

    Added Ews send email and Stop writing submitted values to Mongo and MSSQL extensions

---
+++ src/Sitecore.WffmExtensions/Analytics/Providers/AnalyticsDataProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Sitecore.Abstractions;
+using Sitecore.WFFM.Analytics.Model;
+
+namespace Sitecore.WffmExtensions.Analytics.Providers
+{
+    public class AnalyticsDataProvider : WFFM.Analytics.Providers.AnalyticsDataProvider
+    {
+        private const string ProviderSettingsPrefix = "Sitecore.WffmExtensions.AnalyticsDataProvider.";
+
+        public new void InsertForm(IFormData form)
+        {
+            ISettings settings = new SettingsWrapper();
+
+            var autoStoreValues = settings.GetSetting(ProviderSettingsPrefix + "AutoStoreValues");
+            var autoStoreDisabledReplacementValue = settings.GetSetting(ProviderSettingsPrefix + "AutoStoreDisabledReplacementValue");
+
+            bool storeValuesInSitecore = false;
+
+            if (bool.TryParse(autoStoreValues, out storeValuesInSitecore) && !storeValuesInSitecore)
+            {
+                if (form != null && form.Fields != null && form.Fields.Any())
+                {
+                    form.Fields.ToList().ForEach(field =>
+                    {
+                        if (!string.IsNullOrEmpty(field.Data))
+                        {
+                            field.Data = autoStoreDisabledReplacementValue;
+                        }
+                        field.Value = autoStoreDisabledReplacementValue;
+                    });
+                }
+            }
+
+            base.InsertForm(form);
+        }
+    }
+}
---
+++ src/Sitecore.WffmExtensions/Core/Pipelines/ProcessMessage/ProcessMessage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Exchange.WebServices.Data;
+using Sitecore.Abstractions;
+using Sitecore.Form.Core.Pipelines.ProcessMessage;
+
+namespace Sitecore.WffmExtensions.Core.Pipelines.ProcessMessage
+{
+    public class ProcessMessage
+    {
+        private const string EwsSettingsPrefix = "Sitecore.WffmExtensions.ProcessMessage.SendEwsEmail.";
+
+        public void SendEwsEmail(ProcessMessageArgs args)
+        {
+            ISettings settings = new SettingsWrapper();
+
+            var exchangeUrl = settings.GetSetting(EwsSettingsPrefix + "ExchangeUrl");
+            var exchangeVersion = settings.GetSetting(EwsSettingsPrefix + "ExchangeVersion");
+            var userName = settings.GetSetting(EwsSettingsPrefix + "UserName");
+            var userDomain = settings.GetSetting(EwsSettingsPrefix + "UserDomain");
+            var userPassword = settings.GetSetting(EwsSettingsPrefix + "UserPassword");
+            var fromEmail = settings.GetSetting(EwsSettingsPrefix + "FromEmail");
+
+            ExchangeVersion version;
+            if (!Enum.TryParse(exchangeVersion, out version))
+            {
+                //Default to latest version
+                version = ExchangeVersion.Exchange2013_SP1;
+            }
+
+            var service = new ExchangeService(version)
+            {
+                Url = new Uri(exchangeUrl),
+                Credentials =
+                    !string.IsNullOrEmpty(userDomain)
+                        ? new WebCredentials(userName, userPassword, userDomain)
+                        : new WebCredentials(userName, userPassword)
+            };
+
+            var mail = GetMail(args, service);
+
+            mail.From = fromEmail;
+
+            mail.Send();
+        }
+
+        private EmailMessage GetMail(ProcessMessageArgs args, ExchangeService service)
+        {
+            var mail = new EmailMessage(service);
+
+            if (args.To.Length > 0)
+                args.To.ToString().Split(';').ToList().ForEach(emailAddress => mail.ToRecipients.Add(emailAddress));
+            if (args.CC.Length > 0)
+                args.CC.ToString().Split(';').ToList().ForEach(emailAddress => mail.CcRecipients.Add(emailAddress));
+            if (args.BCC.Length > 0)
+                args.BCC.ToString().Split(';').ToList().ForEach(emailAddress => mail.BccRecipients.Add(emailAddress));
+
+            mail.Subject = args.Subject.ToString();
+
+            mail.Body = args.Mail.ToString();
+
+            args.Attachments.ForEach(attachment => mail.Attachments.AddFileAttachment(attachment.Name, attachment.ContentStream));
+
+            return mail;
+        }
+    }
+}
